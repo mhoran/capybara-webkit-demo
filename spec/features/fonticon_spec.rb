5be1cc1008b84d65cc159daffe17ddcfd2b3194b
Matthew Horan <matt@matthoran.com>
2015-06-14 10:52:17 -0400

    Test fonticons with Bootstrap

--- spec/features/fonticon_spec.rb
+++ spec/features/fonticon_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 feature 'visiting a fonticon page', js: true do
   scenario 'should be able to click the fonticon' do
+    page.driver.block_unknown_urls
+    page.driver.allow_url('maxcdn.bootstrapcdn.com')
+
     visit '/fonticon.html'
 
     page.find('a').click
